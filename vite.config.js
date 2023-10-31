0f6adefb6c69041ca18930df98d8722ffea060c4
daviderivolta-ett <davide.rivolta@grupposcai.com>
2023-10-31 17:10:05 +0100

    New build

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,8 @@ export default defineConfig({
     outDir: 'docs',
     rollupOptions: {
       input: {
-        main: './src/main.js', // Punto di ingresso per la tua pagina principale
-        index: './index.html', // Punto di ingresso per index.html
-        map: './map.html', // Punto di ingresso per map.html
+        index: './index.html',
+        map: './map.html'
       },
     }
   }
