2e8191c60833c79bb09f55fe48aae3540bc987c6
daviderivolta-ett <davide.rivolta@grupposcai.com>
2023-10-31 15:04:04 +0100

    Chips page as index

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -29,7 +29,7 @@ export class Button extends HTMLElement {
         
         this.btn.addEventListener('click', () => {
             localStorage.setItem('selectedTags', this.getAttribute('tags'));
-            window.open('/map', '_blank');
+            window.open('/map.html', '_blank');
         });
 
         // css
