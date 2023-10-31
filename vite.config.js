aa179d486a14a3e1e48f9edc0b6df180212ef1dc
daviderivolta-ett <davide.rivolta@grupposcai.com>
2023-10-31 17:05:40 +0100

    New build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import cesium from 'vite-plugin-cesium';
 export default defineConfig({
   plugins: [cesium()],
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: './src/main.js', // Punto di ingresso per la tua pagina principale
+        index: './index.html', // Punto di ingresso per index.html
+        map: './map.html', // Punto di ingresso per map.html
+      },
+    }
   }
 });
