4feda003647c4c665cc1b4420651f40c9237fe5d
daviderivolta-ett <davide.rivolta@grupposcai.it>
2023-12-15 10:38:15 +0100

    Benched layers style

--- src/components/bench-layer.js
+++ src/components/bench-layer.js
@@ -5,21 +5,33 @@ export class BenchLayer extends HTMLElement {
     }
 
     connectedCallback() {
-        if(!this.hasAttribute('title')) this.setAttribute('title', '');
-        
+        // html
+        if (!this.hasAttribute('title')) this.setAttribute('title', '');
+
         this.shadow.innerHTML =
             `
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-            <span>
-                <label>${this.getAttribute('title')}</label>
-                <span class="icon">
-                    <span class="material-symbols-outlined">delete</span>
+            <div class="chip">
+                <span class="add">
+                    <span class="icon add-icon">
+                        <span class="material-symbols-outlined">add</span>
+                    </span>
+                    <label>${this.getAttribute('title')}</label>
                 </span>
-            </span>
+                <span class="delete">
+                    <span class="icon delete-icon">
+                        <span class="material-symbols-outlined">delete</span>
+                    </span>
+                </span>
+            </div>
             `
-        ;
+            ;
 
-
+        // css
+        const style = document.createElement('link');
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', './css/bench-layer.css');
+        this.shadow.append(style);
     }
 }
 
